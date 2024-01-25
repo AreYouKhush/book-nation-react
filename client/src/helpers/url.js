d8a98df2a1f7651b1a66782ae55114aed2063933
AreYouKhush <khushal9587@gmail.com>
2024-01-25 15:48:55 +0530

    railway deploy

--- client/src/helpers/url.js
+++ client/src/helpers/url.js
@@ -1,2 +1,8 @@
-export const url = "https://booknation-api.onrender.com/";
+// localhost
 // export const url = "http://localhost:3000/";
+
+// render
+// export const url = "https://booknation-api.onrender.com/";
+
+// railway
+export const url = "https://book-nation.up.railway.app/";
